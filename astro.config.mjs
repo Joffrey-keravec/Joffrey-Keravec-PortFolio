83be9cb59db0309715c81ead8fea121433c921d3
keravec <keravecjoffrey@gmail.com>
2024-02-11 23:57:52 +0100

    0.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Joffrey-keravec.github.io',
-  base: '/JoffreyKeravecPortFolio',
+    
 });
