e2a028451f402c4153fd3be51ce02eb757343598
keravec <keravecjoffrey@gmail.com>
2024-02-12 01:04:56 +0100

    debug0.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://joffrey-keravec.github.io/JoffreyKeravecPortFolio/',
+    site: 'https://joffrey-keravec.github.io/',
   
 });
 
